def7eaa8b879b362c959ac7525e2dc5a96d0a274
luirusso <luirusso_@hotmail.it>
2021-10-28 16:31:52 +0200

    added files to right folders

---
+++ js-snacks-blocco-2/jsnack5/index.js
@@ -0,0 +1,15 @@
+/**
+ * Crea due array che hanno un numero di elementi diversi.
+Aggiungi elementi casuali all’array che ha meno elementi,
+fino a quando ne avrà tanti quanti l’altro.
+ */
+
+
+const group1 = [1, 2, 3, 4, 5, 6 ,7 ,8 ,9, 10];
+console.log(group1)
+const group2 = [1, 2, 3, 4, 5, 6 ,7 ,8];
+console.log(group2);
+
+const size = group2;
+console.log(size);
+
